d588957cc95e2ad42fbdbf3f40aab8ef585794e8
Travis Beatty <travisby@gmail.com>
2021-10-24 11:00:22 -0400

    Include missing disable SystemInit

--- soc/arm/nuvoton_numicro/m2351/soc.c
+++ soc/arm/nuvoton_numicro/m2351/soc.c
@@ -12,7 +12,12 @@ void z_arm_platform_init(void)
 	SYS_UnlockReg();
 
 	/* system clock init */
-	SystemInit();
+    // XXX:
+    // SystemInit uses a symbol `__Vectors` when working with the VTOR setup
+    // https://github.com/OpenNuvoton/M2351BSP/blob/5e272d7a5980344c5e24de8a598d47f5e5a14e55/Library/Device/Nuvoton/M2351/Source/system_M2351.c#L393-L409
+    // This errors because __Vectors is some startup code in Assembly that we did not include
+    // And considering Zephyr has code for VTOR already, it's probably(?) handled elsewhere
+	// SystemInit();
 
     // XXX: Clock setup?
     // UART works w/o anything else here
