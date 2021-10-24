e8c1382b3841626b36e8b7deca1a4933dad6ca92
Travis Beatty <travisby@gmail.com>
2021-10-24 10:24:42 -0400

    Added simple implementation of NuMaker PFM-M2351

---
+++ soc/arm/nuvoton_numicro/m2351/soc.h
@@ -0,0 +1,12 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_SOC_ARM_NUVOTON_M2351_SOC_H_
+#define ZEPHYR_SOC_ARM_NUVOTON_M2351_SOC_H_
+
+#include <sys/util.h>
+#include <devicetree.h>
+#include <NuMicro.h>
+
+#endif /* ZEPHYR_SOC_ARM_NUVOTON_M2351_SOC_H_*/
